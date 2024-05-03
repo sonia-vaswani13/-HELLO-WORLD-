8764cafab1f758526fcc6ba3322913446902f193
sonia-vaswani13 <163179929+sonia-vaswani13@users.noreply.github.com>
2024-05-03 13:39:44 -0700

    Hello-world  print by typscript
    Hello-world  print by typscript

---
+++ hello.js
@@ -0,0 +1,4 @@
+console.log(" hello wold");
+let name = "hello world";
+console.log(name);
+export {};
---
+++ hello.ts
@@ -0,0 +1,3 @@
+console.log( " hello wold")
+let  name : string ="hello world";
+console.log ( name);
